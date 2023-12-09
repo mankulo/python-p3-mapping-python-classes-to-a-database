a0e8a85fee0400825892d37b366ea76ca3dd1df2
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-12-10 00:34:03 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -5,4 +5,4 @@ from song import Song
 
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+   import ipdb; ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,56 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    all = []
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def create(cls, name, album):
+        sql = """
+            INSERT INTO songs(name, album)
+            VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (name, album))
+        CONN.commit()
+        song = cls(name, album)
+        song.id = CURSOR.lastrowid
+        return song
+    
+    def save(self):
+        sql = """
+            INSERT INTO songs(name, album)
+            VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def new_from_db(cls,row):
+        song = cls(row[1],row[2])
+        song.id = row[0]
+
+    @classmethod
+    def all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all]
